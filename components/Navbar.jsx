9ce6db01379cd8482d56e0f322745f03c453de66
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-07 16:33:03 +0600

    change add navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useState, useEffect, useContext } from "react";
+import { useRouter } from "next/router";
+import { useTheme } from "next-themes";
+import Image from "next/image";
+import Link from "next/link";
+import images from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { theme, setTheme } = useTheme();
 
-export default Navbar
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b darK:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1 ">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="felxCenter md:hidden cursor-pointer">
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              alt="logo"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
+              Cryptoket
+            </p>
+          </div>
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
