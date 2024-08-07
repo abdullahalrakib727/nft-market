0d96b89ccc2761b346e1f8fc91775277b5762762
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-07 16:46:54 +0600

    change added custom theme toggle

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b darK:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1 ">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
-          <div className="felxCenter md:hidden cursor-pointer">
+          <div className="flexCenter md:hidden cursor-pointer">
             <Image
               src={images.logo02}
               objectFit="contain"
@@ -26,6 +26,31 @@ const Navbar = () => {
             </p>
           </div>
         </Link>
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              alt="logo"
+              width={32}
+              height={32}
+              className="cursor-pointer"
+            />
+          </div>
+        </Link>
+      </div>
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input type="checkbox" className="checkbox" id="checkbox" />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
+          >
+            <i className="fas fa-sun" />
+            <i className="fas fa-moon" />
+            <div className="w-3 h-3 bg-white absolute rounded-full ball" />
+          </label>
+        </div>
       </div>
     </nav>
   );
