443ed48c36ea61bc1a9a63170a4588f51ebdd6e4
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-08 11:07:37 +0600

    Change Add Button component and update Navbar component

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -4,12 +4,70 @@ import { useTheme } from "next-themes";
 import Image from "next/image";
 import Link from "next/link";
 import images from "../assets";
+import { Button } from "./";
+
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (i) => {
+    switch (i) {
+      case 0:
+        return "/";
+      case 1:
+        return "/created-nfts";
+      case 2:
+        return "/my-nfts";
+      default:
+        return "/";
+    }
+  };
+  return (
+    <ul
+      className={`list-none flexCenter flex-row${
+        isMobile && "flex-col h-full"
+      }`}
+    >
+      {["Explore NFTs", "Listed NFTs", "My NFTs"].map((item, i) => (
+        <li
+          key={i}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? "dark:text-white text-nft-black-1"
+              : "dark:text-nft-gray-3 text-nft-gray-2"
+          }`}
+        >
+          <Link href={generateLink(i)}>{item}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+const ButtonGroup = ({setActive, router}) => {
+  const hasconnected = true;
+
+  return hasconnected ? (
+    <Button btnName="Create" classStyles="mx-2 rounded-xl "
+ handleClick  = { ()=>{
+  setActive("Create")
+  router.push("/create-nft")
+ }}
+    > </Button>
+  ) : (
+    <Button btnName="Connect" classStyles="mx-2 rounded-xl "> </Button>
+  );
+};
 
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
+  const [active, setActive] = useState("Explore NFTs");
+
+  // ! Theme not working, will fix this later
 
   return (
-    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b darK:bg-nft-dark bg-white dark:border- nft-black-1 border-nft-gray-1 ">
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border- nft-black-1 border-nft-gray-1 ">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
           <div className="flexCenter md:hidden cursor-pointer">
@@ -53,6 +111,12 @@ const Navbar = () => {
             <i className="fas fa-moon" />
             <div className="w-3 h-3 bg-white absolute rounded-full ball" />
           </label>
+        </div>
+      </div>
+      <div className="md:hidden flex">
+        <MenuItems active={active} setActive={setActive} />
+        <div className="ml-4 ">
+          <ButtonGroup setActive={setActive} router={router}/>
         </div>
       </div>
     </nav>
