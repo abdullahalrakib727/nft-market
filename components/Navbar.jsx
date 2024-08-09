9961369398f4e4a1a62eecca0175a8ee66691d65
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-09 09:41:22 +0600

    change added send email option

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -71,8 +71,6 @@ const Navbar = () => {
   const [active, setActive] = useState("Explore NFTs");
   const [isOpen, setIsOpen] = useState(false);
 
-  console.log({ theme });
-
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1 ">
       <div className="flex flex-1 flex-row justify-start">
