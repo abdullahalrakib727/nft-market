6f1df50acf98e9d4c925570445610f9959aca125
Abdullah Al Rakib <138372162+alrakib1@users.noreply.github.com>
2024-08-09 16:22:10 +0600

    change banner completed

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,9 @@ const App = ({ Component, pageProps }) => {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <div className="dark:bg-nft-dark bg-white min-h-screen">
         <Navbar />
-        <Component {...pageProps} />
+        <div className="pt-65">
+          <Component {...pageProps} />
+        </div>
         <Footer />
       </div>
       <Script
